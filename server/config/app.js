6b76d89c13b7d92e6920a89dae7da43c5a220641
oosoriodr <113384970+oosoriodr@users.noreply.github.com>
2022-11-09 14:16:09 -0500

    Bootstrap & Navbar added
    Extra packages installed. Header, footer and partials views configured

--- server/config/app.js
+++ server/config/app.js
@@ -18,6 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../../node_modules/jquery/dist/')));
+app.use(express.static(path.join(__dirname, '../../node_modules/bootstrap/dist/')));
 
 //Set up the router
 app.use('/', indexRouter);
