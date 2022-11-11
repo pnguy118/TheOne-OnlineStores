956c5adc5b4596f9fd32294010408c182381a085
Adam <npham20@my.centennialcollge.ca>
2022-11-10 20:05:00 -0500

    modifying list of Store and add mongodb server

---
+++ server/routes/store.js
@@ -0,0 +1,81 @@
+let express = require('express');
+let router = express.Router();
+
+//Create a reference to the Store model
+let Store = require('../models/store');
+
+/* GET display store list page. */
+router.get('/', function (req, res, next) {
+  res.render('stores/list', { title: 'Products' });
+});
+
+/* GET display store add page */
+router.get('/add', function (req, res, next) {
+  res.render('stores/add', { title: 'Add Product' });
+});
+
+/* POST add a new store model */
+router.post('/add', function (req, res, next) {
+  let newStore = Store({
+    "storeName": req.body.storeName,
+    "owner": req.body.owner,
+    "type": req.body.type,
+    "location": req.body.location,
+    "about": req.body.about,
+    "rate": req.body.rate,
+    "review": req.body.review
+  });
+
+  Store.create(newStore, (err, store) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    }
+    else {
+      res.redirect('/store-list');
+    }
+  });
+});
+
+/* GET display the edit store page */
+router.get('/edit/:id', function (req, res, next) {
+  let id = req.params.id;
+  Store.findById(id, (err, storeToEdit) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    }
+    else {
+      res.render('/store/edit', { title: "Edit Store", store: storeToEdit });
+    }
+  });
+});
+
+/* POST update store */
+router.post('/edit/:id', function (req, res, next) {
+
+  let id = req.params.id;
+
+  let updatedStore = Store({
+    "_id": id,
+    "storeName": req.body.storeName,
+    "owner": req.body.owner,
+    "type": req.body.type,
+    "location": req.body.location,
+    "about": req.body.about,
+    "rate": req.body.rate,
+    "review": req.body.review
+  });
+
+  Store.updateOne({ _id: id }, updatedStore, (err) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    }
+    else {
+      res.redirect('/store-list');
+    }
+  });
+
+});
+module.exports = router;
